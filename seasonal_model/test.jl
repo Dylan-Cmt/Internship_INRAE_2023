6b15b1de9bb8a39edea79087d2c562a7142bee8e
Dylan-Cmt <cormet.dylan@gmail.com>
2023-06-07 14:20:43 +0200

    update comments

--- seasonal_model/test.jl
+++ seasonal_model/test.jl
@@ -5,7 +5,12 @@ using StaticArrays                                                  # for @SVect
 using Parameters                                                    # for @with_kw
 ##############################################    PROBLEM INITIALISATION    ################################################################
 
+"""
+    struct Growing
 
+Contains the model informations for the growing season,
+with few default values.
+"""
 @with_kw struct Growing
     etat0::SVector{3,Float64} = @SVector [0.01, 1.0, 0.0]
     params::Vector{Float64}
@@ -14,6 +19,12 @@ using Parameters                                                    # for @with_
     model::Function = modelg
 end
 
+"""
+    struct Winter
+
+Contains the model informations for the winter season,
+with few default values.
+"""
 @with_kw struct Winter
     params::Union{Float64,Vector{Float64}}
     tspan::Tuple{Float64,Float64}
@@ -22,7 +33,12 @@ end
     convertIP::Float64
 end
 
-# accumulation of results (also type missing to plot a discontinuity)
+"""
+    mutable struct Result
+
+Contains the accumulation of results.
+Can also contains Missing type to include discontinuity.
+"""
 @with_kw mutable struct Result
     all_P::Vector{Union{Missing,Float64}} = []
     all_S::Vector{Union{Missing,Float64}} = []
@@ -30,7 +46,11 @@ end
     all_t::Vector{Union{Missing,Float64}} = []
 end
 
-# model for the growing season
+"""
+    modelg(u::SVector{3,Float64}, params, t)
+
+It is the model for the growing season.
+"""
 function modelg(u::SVector{3,Float64}, params, t)
     α, β, Λ, Θ = params                                             # unpack the vectors into scalar
     p, s, i    = u
@@ -40,7 +60,11 @@ function modelg(u::SVector{3,Float64}, params, t)
     @SVector [dp, ds, di]                                           # return a new vector
 end
 
-# model for the winter season
+"""
+    modelw(u::SVector{3,Float64}, params, t)
+
+It is the model for the winter season.
+"""
 function modelw(u::SVector{3,Float64}, params, t)
     μ       = params                                                # unpack the vectors into scalar
     p, s, i = u
@@ -50,90 +74,113 @@ function modelw(u::SVector{3,Float64}, params, t)
     @SVector [dp, ds, di]                                           # return a new vector
 end
 
-function simule(years, growing::Growing, winter::Winter, res::Result, plot=true; kwarg...)
+"""
+    simule(years, growing::Growing, winter::Winter, res::Result; kwarg...)
+
+Simulates x years of alternation between growing and winter seasons.
+"""
+function simule!(years, growing::Growing, winter::Winter, res::Result; kwarg...)
     
+    # collect the tspans
     tspang = growing.tspan
     tspanw = winter.tspan
-    # growing season
+
+    # GROWING SEASON
+    # solve the ODE problem for a first growing season
     problemg  = ODEProblem(growing.model, growing.etat0, tspang, growing.params, saveat=growing.pas)
     solutiong = solve(problemg)
-
+    # collect the results
     res.all_P = vcat(res.all_P, solutiong[1, :])
     res.all_S = vcat(res.all_S, solutiong[2, :])
     res.all_I = vcat(res.all_I, solutiong[3, :])
     res.all_t = vcat(res.all_t, solutiong.t)
 
-
-
+    # this variable will never change so let's put it before the loop
     s0g = growing.etat0[2]
+
+    # simulation for the rest of the time
     for _ in 1:years-1
 
-        # winter season
+        # WINTER SEASON
+        # collect the last values to get new initial conditions
         p_fin_g, s_fin_g, i_fin_g = last(solutiong)
+        # new initial conditions
         p0w = p_fin_g + winter.convertIP * i_fin_g
         s0w = 0.0
         i0w = 0.0
+        # encapsulation 
         etat0 = @SVector [p0w, s0w, i0w]
+        # solve the ODE problem for winter season
         problemw = ODEProblem(winter.model, etat0, tspanw, winter.params, saveat=winter.pas)
         solutionw = solve(problemw)
-
+        # collect the results
         res.all_P = vcat(res.all_P, missing, solutionw[1, 2:end])
         res.all_S = vcat(res.all_S, missing, solutionw[2, 2:end-1], missing)
         res.all_I = vcat(res.all_I, missing, solutionw[3, 2:end])
         res.all_t = vcat(res.all_t, solutionw.t)
-        
+        # update tspan of winter season for the next year
         tspanw = tspanw .+ 365
-        
-        # growing season
+
+        # GROWING SEASON
+        # update tspan of growing season 
         tspang = tspang .+ 365
+        # collect the last values to get new initial conditions
         p_fin_w, s_fin_w, i_fin_w = last(solutionw)
+        # new initial conditions
         p0g = p_fin_w
         i0g = 0.0
+        # encapsulation
         etat0 = @SVector [p0g, s0g, i0g]
+        # solve the ODE problem for growing season
         problemg = ODEProblem(growing.model, etat0, tspang, growing.params, saveat=growing.pas)
         solutiong = solve(problemg)
-
+        # collect the results
         res.all_P = vcat(res.all_P, solutiong[1, :])
         res.all_S = vcat(res.all_S, solutiong[2, :])
         res.all_I = vcat(res.all_I, solutiong[3, :])
         res.all_t = vcat(res.all_t, solutiong.t) 
     end
 
-    if plot
-        # convert days into years
-        year = res.all_t ./ winter.tspan[2]
+    # PLOT 
 
-        # plot I
-        p1 = Plots.plot(year, res.all_I,
-            label="\$I\$",
-            legend=:topleft,
-            c=:red,
-            xlabel="Years",
-            ylabel="\$I(t)\$",
-            linestyle=:solid,
-            ylims=[0, s0g / 3])
+    # convert days into years
+    t = res.all_t ./ winter.tspan[2]
 
-        # plot I and P in the same plot
-        p1 = Plots.plot!(twinx(), year, res.all_P,
-            c=:black,
-            label="\$P\$",
-            legend=:topright,
-            ylabel="\$P(t)\$",
-            linestyle=:dashdotdot,
-            ylims=[0, winter.convertIP * s0g / 3])
+    # plot I
+    p1 = Plots.plot(t, res.all_I,
+        label="\$I\$",
+        legend=:topleft,
+        c=:red,
+        xlabel="Years",
+        ylabel="\$I(t)\$",
+        linestyle=:solid,
+        ylims=[0, s0g / 3])
 
-        # plot S
-        p2 = Plots.plot(year, res.all_S, ylims=[0, s0g], label=false, ylabel="\$S(t)\$", title="Airborne model")
+    # plot I and P in the same plot
+    p1 = Plots.plot!(twinx(), t, res.all_P,
+        c=:black,
+        label="\$P\$",
+        legend=:topright,
+        ylabel="\$P(t)\$",
+        linestyle=:dashdotdot,
+        ylims=[0, winter.convertIP * s0g / 3])
 
-        # subplot S and (P/I)
-        Plots.plot(p2, p1, layout=(2, 1), xlims=[0, years])
-    else
-        return [res.all_t, res.all_P, res.all_S, res.all_I]
-    end
+    # plot S
+    p2 = Plots.plot(t, res.all_S,
+        ylims=[0, s0g],
+        label=false,
+        ylabel="\$S(t)\$",
+        title="Airborne model")
+
+    # subplot S and (P/I)
+    Plots.plot(p2, p1,
+        layout=(2, 1),
+        xlims=[0, years])
+    
 end    
 
 
-##############################################    TEST   ################################################################
+#######################################################    TEST   ################################################################
 
 t_0 = 0
 τ = 184                                                             # growing season length (in days)
@@ -155,4 +202,4 @@ growing = Growing(params=params, tspan=(t_0, t_transi))
 winter = Winter(params=μ, tspan=(t_transi, t_fin), convertIP=π)
 res = Result()
 
-simule(5, growing, winter, res, plot=true)
+simule!(10, growing, winter, res)
