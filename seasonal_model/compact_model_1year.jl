40c8ed02fb3715b6c24bb5417f03f9f026b90bb7
Dylan-Cmt <cormet.dylan@gmail.com>
2023-06-08 08:58:47 +0200

    add some struct and a simule function

--- seasonal_model/compact_model_1year.jl
+++ seasonal_model/compact_model_1year.jl
@@ -2,45 +2,107 @@
 using DifferentialEquations                                         # for ODEProblem and solve
 using Plots                                                         # for plot
 using StaticArrays                                                  # for @SVector 
+using Parameters                                                    # for @with_kw
 
-# time
-t_0      = 0
-τ        = 184                                                             
-Τ        = 365                                                            
-t_transi = Τ - τ
-t_fin    = Τ
 
-#tspan
-pas_t  = 1
-tspang = (t_0, t_transi)
-tspanw = (t_transi, t_fin)
+"""
+    struct Growing
+
+Contains the model informations for the growing season,
+with few default values.
+"""
+@with_kw struct Growing
+    etat0::SVector{2,Float64} = @SVector [0.01, 1.0, 0.0]
+    params::Vector{Float64}
+    others_params::Vector{Float64}
+    tspan::Tuple{Float64,Float64}
+    pas = 1
+    model::Function = modelg
+end
+
+
+"""
+    mutable struct Result
+
+Contains the accumulation of results.
+Can also contains Missing type to include discontinuity.
+"""
+@with_kw mutable struct Result
+    all_P::Vector{Vector{Float64}} = []
+    all_S::Vector{Vector{Float64}} = []
+    all_I::Vector{Vector{Float64}} = []
+    all_t::Vector{Vector{Float64}} = []
+end
+
+
+"""
+    modelg(u::SVector{3,Float64}, params, t)
+
+It is the model for the growing season.
+"""
+function modelg(u::SVector{2,Float64}, params, t)
+    α, β = params                                                   # unpack the vectors into scalar
+    s, i = u
+    ds = -β * s * i                                                 # dot s
+    di = +β * s * i - α * i                                         # dot i
+    @SVector [ds, di]                                               # return a new vector
+end
+
+"""
+    simule(years, growing::Growing, winter::Winter, res::Result; kwarg...)
+
+Simulates x years of alternation between growing and winter seasons.
+"""
+function simule(years, growing::Growing; kwarg...)
+    # Creat a Result type to collect results
+    res = Result()
+    # collect tspan
+    tspan = growing.tspan
+    # collect others parameters
+    @unpack θ, π, μ, λ = growing.others_params
+    # inittial conditions
+    etat0 = growing.etat0
+    @unpack s0, i0 = etat0                                          # susceptible and infected host plant density
+    problem = ODEProblem(modelg, etat0, tspan, paramsg, saveat=pas_t)
+    solution = solve(problem)
+    # collect the results
+    res.all_S = push!(res.all_S, solutiong[1, :])
+    res.all_I = push!(res.all_I, solutiong[2, :])
+    res.all_t = push!(res.all_t, solutiong.t)
+
+    # simulation for the rest of the time
+    for _ in 1:years-1
+        # update tspan of growing season 
+        tspan = tspan .+ 365
+        # collect the last values to get new initial conditions
+        s_fin_g, i_fin_g = last(solution)
+        # new initial conditions
+        s0g = s0 * exp(-θ * π * exp(-μ(Τ - τ)) * i_fin_g / λ)
+        i0g = s0 * (1 - exp(-θ * π * exp(-μ(Τ - τ)) * i_fin_g / λ))
+        # encapsulation
+        etat0 = @SVector [s0g, i0g]
+        # solve the ODE problem for growing season
+        problemg = ODEProblem(growing.model, etat0, tspan, growing.params, saveat=growing.pas)
+        solutiong = solve(problemg)
+        # collect the results
+        res.all_P = vcat(res.all_P, solutiong[1, :])
+        res.all_S = vcat(res.all_S, solutiong[2, :])
+        res.all_I = vcat(res.all_I, solutiong[3, :])
+        res.all_t = vcat(res.all_t, solutiong.t)
+    end
+end
 
 ##############################################    GROWING SEASON: year 1    ################################################################
 
 
 # initial conditions
-s0 = 1.0                                                            # susceptible host plant density
-i0 = 0.0                                                            # infected host plant density
+s0 = 1.0                                                            
+i0 = 0.0                                                            
 # encapsulation 
 etat0 = @SVector [s0, i0]
 
-# parameters
-α = 0.024                                                           # infected host plants removal rate per day
-β = 0.04875                                                         # secondary infection rate per day per host plant unit
-paramsg = [α, β]
-
-
-# model for the growing season
-function modelg(u::SVector{2,Float64}, params, t)
-    α, β = params                                                   # unpack the vectors into scalar
-    s, i = u
-    ds   = - β * s * i                                              # dot s
-    di   = + β * s * i - α * i                                      # dot i
-    @SVector [ds, di]                                               # return a new vector
-end
-
-problemg  = ODEProblem(modelg, etat0, tspang, paramsg, saveat=pas_t)
-solutiong = solve(problemg)
+problem  = ODEProblem(modelg, etat0, tspang, paramsg, saveat=pas_t)
+solution = solve(problem)
 
 
 
@@ -54,18 +116,19 @@ solutiong = solve(problemg)
 
 
 # winter season data recovery
-s_fin_g, i_fin_g = last(solutiong)
+s_fin_g, i_fin_g = last(solution)
 
 # new initial conditions
 s1 = s0 * exp(-θ * π * exp(-μ(Τ - τ)) * i_fin_g / λ)
 i1 = s0*(1 - exp(-θ * π * exp(-μ(Τ - τ)) * i_fin_g / λ))
 # encapsulation 
-etat0w = @SVector [s1, i1]
+etat0 = @SVector [s1, i1]
 
-problemg = ODEProblem(modelg, etat1, tspang .+ 360, paramsg, saveat=pas_t)
-solutiong = solve(problemg)
+problem = ODEProblem(modelg, etat1, tspan .+ 360, paramsg, saveat=pas_t)
+solution = solve(problem)
 
-##############################################    GROWING SEASON: year 2    ################################################################
+
+
 
 #=
 # plot S
@@ -86,3 +149,28 @@ plot(p1, p2,
     layout=(2, 1))
 title!("Simulation du modèle airborne élaboré", subplot=1)
 =#
+
+##############################################    TEST    ################################################################
+
+# time
+t_0 = 0
+τ = 184
+Τ = 365
+t_transi = Τ - τ
+t_fin = Τ
+
+#tspan
+pas = 1
+tspan = (t_0, t_transi)
+
+# parameters
+α = 0.024                                                           # infected host plants removal rate per day
+β = 0.04875                                                         # secondary infection rate per day per host plant unit
+params = [α, β]
+# others parameters
+θ = 
+π = 
+μ = 
+λ = 
+
+growing = Growing(params=params, tspan=(t_0, t_transi))
