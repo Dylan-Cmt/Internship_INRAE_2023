7a948dace52ddc46ff6699cec081be7751c6124b
Dylan-Cmt <cormet.dylan@gmail.com>
2023-06-08 10:14:50 +0200

    remove popfirst

--- some_tests/seasonal_discontinuity.jl
+++ some_tests/seasonal_discontinuity.jl
@@ -11,18 +11,16 @@ y3 = x3 .^(1/3)
 y4 = x4 .^(1/4)
 
 
-t = [[]]                    # initialize a vector of vectors
+t = []                      # initialize a vector (of vectors)
 push!(t, x1)                # add each vector of time
 push!(t, x2)    
 push!(t, x3)
 push!(t,x4)
-popfirst!(t)                # pop the empty first
 
-x = [[]]                    # again
+x = []                    # again
 push!(x,y1)
 push!(x, y2)
 push!(x, y3)
 push!(x,y4)
-popfirst!(x)
 
 plot(t,x)
