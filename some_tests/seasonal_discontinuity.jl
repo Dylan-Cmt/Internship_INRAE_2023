5cfec56c078d35ec80ef333d44b9cc4a33c0d379
Dylan-Cmt <cormet.dylan@gmail.com>
2023-06-08 09:52:29 +0200

    update

--- some_tests/seasonal_discontinuity.jl
+++ some_tests/seasonal_discontinuity.jl
@@ -1,11 +1,28 @@
 using Plots 
 
-x1=1:.1:5
-x2=5:.1:15
+x1 = 1:.1:5
+x2 = 5:.1:15
 x3 = 15:.1:30
+x4 = 40:.1:45
 
 y1 = x1
 y2 = sqrt.(x2) 
 y3 = x3 .^(1/3)
+y4 = x4 .^(1/4)
 
-plot([x1, x2, x3],[y1, y2, y3])
+
+t = [[]]                    # initialize a vector of vectors
+push!(t, x1)                # add each vector of time
+push!(t, x2)    
+push!(t, x3)
+push!(t,x4)
+popfirst!(t)                # pop the empty first
+
+x = [[]]                    # again
+push!(x,y1)
+push!(x, y2)
+push!(x, y3)
+push!(x,y4)
+popfirst!(x)
+
+plot(t,x)
