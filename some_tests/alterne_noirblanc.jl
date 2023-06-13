167a2f162a29f2e8d06a79d72f4bc567f45ffe00
Dylan-Cmt <cormet.dylan@gmail.com>
2023-06-13 13:51:59 +0200

    an example of plot with alternating white and gray stripes

---
+++ some_tests/alterne_noirblanc.jl
@@ -0,0 +1,18 @@
+using Plots
+
+x = 1:1:365*2
+y = cos.(x/50)
+
+vspan([1, 365 / 2, 365, 2 * 365 / 3, 2 * 365],
+    color=:lightgray,
+    label="growing season")
+
+vspan!([365 / 2, 365, 2*365/3, 2*365],
+    color=:white,
+    label="winter season")
+    
+plot!(x,y,
+    label=false,
+    c=:black,
+    xlabel="x",
+    ylabel="y")
