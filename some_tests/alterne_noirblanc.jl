04190d192518d8b0c5ee49151d261a78840367b2
Ludovic Mailleret <ludovic.mailleret@inrae.fr>
2023-06-13 15:15:07 +0200

    remove white

--- some_tests/alterne_noirblanc.jl
+++ some_tests/alterne_noirblanc.jl
@@ -3,13 +3,13 @@ using Plots
 x = 1:1:365*2
 y = cos.(x/50)
 
-vspan([1, 365 / 2, 365, 2 * 365 / 3, 2 * 365],
+vspan([0, 365 / 2, 365, 3 * 365 / 2, 2 * 365],
     color=:lightgray,
     label="growing season")
 
-vspan!([365 / 2, 365, 2*365/3, 2*365],
-    color=:white,
-    label="winter season")
+# vspan!([365 / 2, 365, 2*365/3, 2*365],
+#     color=:white,
+#     label="winter season")
     
 plot!(x,y,
     label=false,
