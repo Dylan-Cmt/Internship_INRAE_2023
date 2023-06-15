7b48847257c68bdbe3e6d829f9a6cbae69fa3196
Dylan-Cmt <cormet.dylan@gmail.com>
2023-06-15 09:01:44 +0200

    construct strips for n years

--- some_tests/alterne_noirblanc.jl
+++ some_tests/alterne_noirblanc.jl
@@ -1,17 +1,30 @@
 using Plots
 
-x = 1:1:365*2
-y = cos.(x/50)
+τ = 120
+Τ = 365
+n = 5
 
-vspan([0, 365 / 2, 365, 3 * 365 / 2, 2 * 365],
+x = 1:1:Τ*n
+y = cos.(x / 50)
+
+v = [[0, τ]]
+vspan(v[1] ./ Τ,
     color=:lightgray,
     label="growing season")
 
-# vspan!([365 / 2, 365, 2*365/3, 2*365],
-#     color=:white,
-#     label="winter season")
-    
-plot!(x,y,
+for i in 1:n-1
+    u = [i * Τ, i * Τ + τ]
+    push!(v, u)
+end
+
+
+
+vspan!(v[2:end] ./ Τ,
+    color=:lightgray,
+    label=false)
+
+annee = x ./ Τ
+plot!(annee, y,
     label=false,
     c=:black,
     xlabel="x",
