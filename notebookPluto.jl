57afc49b030c95ebf4575095c90d24120c322a4d
Dylan-Cmt <cormet.dylan@gmail.com>
2023-06-22 11:11:40 +0200

    test PlutoNotebook

---
+++ notebookPluto.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a4fc28a4-10dc-11ee-30fe-c9bb29bd23bf
+a = 1
+
+# ╔═╡ Cell order:
+# ╠═a4fc28a4-10dc-11ee-30fe-c9bb29bd23bf
