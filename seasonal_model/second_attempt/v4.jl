385b75dc14652b91d1fafe63bb1fdd6d7d6be159
Dylan-Cmt <cormet.dylan@gmail.com>
2023-07-19 11:45:02 +0200

    test git

--- seasonal_model/second_attempt/v4.jl
+++ seasonal_model/second_attempt/v4.jl
@@ -545,7 +545,7 @@ Test = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.9.0"
+julia_version = "1.9.1"
 manifest_format = "2.0"
 project_hash = "cddd8949ffa1d3983bee233489a1180ec26edb9e"
 
@@ -2334,7 +2334,7 @@ version = "0.15.1+0"
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.7.0+0"
+version = "5.8.0+0"
 
 [[deps.libfdk_aac_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
